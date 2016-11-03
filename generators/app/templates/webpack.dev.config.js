d5b539ed9255477f19b57e176770e8f941ddbebd
Mark Beukers <mark@afrotoss.com>
2016-11-03 11:20:14 +0800

    Downgrade (#6)
    * try to fix AppVeyor
    
    * regress to foundation 5
    
    * hot reload now enabled
    
    * another AV fix
    
    * 0.3.1 version bump

--- generators/app/templates/webpack.dev.config.js
+++ generators/app/templates/webpack.dev.config.js
@@ -1,6 +1,6 @@
 var cfg = require('./webpack.config.js')
 
 // Webpack Dev server specific config
-cfg.output.publicPath = "{dev_url}"
+cfg.output.publicPath = "http://localhost:8080/"
 
 module.exports = cfg
